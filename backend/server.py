8bfa2aa9e0b480d55dfe948c9b70accbaf29d4de
hdphuong <63543383+hdphuong@users.noreply.github.com>
2022-04-07 20:33:35 +1000

    ?

--- backend/server.py
+++ backend/server.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+@app.route('/api', methods=['GET'])
+def api():
+    return {
+        'userId': 1,
+        'title': 'Flask React API',
+        'completed': False,
+    }
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
